db0ab1462df2b816c5caff8ee979787e466f6a47
Derek <derekdvalerio@gmail.com>
2025-09-21 20:53:00 +0800

    created modules folder and todo clas on todo.js

---
+++ src/modules/todo.js
@@ -0,0 +1,10 @@
+export class Todo {
+  constructor(title, description, dueDate, priority = "normal", completed = false) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.completed = completed;
+  }
+}
